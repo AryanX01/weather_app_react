74f2f007c8a0a6fa0f7210c2c2ec92a72d92848b
AryanX01 <bansalaryan08032004@gmail.com>
2024-09-17 15:09:37 +0530

    initial commit

---
+++ src/api.jsx
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'x-rapidapi-key': '373611889fmsh0cfbd160382f1a6p109181jsn308c0d3e1b7a',
+		'x-rapidapi-host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "f9029734ac52b82adc1eaad2351182d0";
